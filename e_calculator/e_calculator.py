5789d7af3e99c50badf44088ebb6f099c33ca08e
Brent Follin <brentfollin@gmail.com>
2020-05-11 22:02:31 -0700

    fix docstrings

--- e_calculator/e_calculator.py
+++ e_calculator/e_calculator.py
@@ -1,5 +1,6 @@
-import numpy as np 
+import numpy as np
 from matplotlib import pyplot as plt
+
 
 class CalculateE:
     """
@@ -14,9 +15,9 @@ class CalculateE:
 
     chain: list[float]
         The MCMC chain. Set by the ``.run_mcmc()`` method.
-    
+
     a_array: list[float]
-        An array of uniformly sampled guesses for ``e``. Set by the 
+        An array of uniformly sampled guesses for ``e``. Set by the
         ``.uniformly_sample_space()`` method.
 
     posterior: list[float]
@@ -26,37 +27,28 @@ class CalculateE:
     Examples
     --------
     Calculate the best value using metropolis hastings:
-    
-    >>> calculator = calculate_e.CalculateE(nsamples=200)
+
+    >>> import e_calculator
+    >>> calculator = e_calculator.CalculateE(nsamples=200)
     >>> calculator.run_mcmc(1.01, 10) 
     >>> calculator.plot(mcmc=True)
-    
+
     Calculate the best value using a uniform sampling of parameter space:
-    
-    >>> calculator = calculate_e.CalculateE(nsamples=200)
+
+    >>> import e_calculator
+    >>> calculator = e_calculator.CalculateE(nsamples=200)
     >>> calculator.uniformly_sample_space(1.01, 7)  
     >>> calculator.plot(mcmc=False)
     """
+
     def __init__(self, nsamples=100):
-        """
-        initialize calculate_e object with nsamples of data
 
-        Parameters
-        ----------
-        nsamples: positive int
-            number of data samples
-
-        Returns
-        -------
-        : None
-        """
         self.nsamples = nsamples
         self.ntrials = 1000
         self.data = self._generate_data(nsamples)
         self.a_array = None
         self.posterior = None
         self.chain = None
-        
 
     def _generate_data(self, nsamples):
         '''
@@ -74,7 +66,6 @@ class CalculateE:
         '''
         return np.random.standard_normal(nsamples)
 
-            
     def calculate_posterior(self, a):
         '''
         Calculate the posterior by calculating P(a|x) for each x
@@ -114,7 +105,8 @@ class CalculateE:
             ax.set_xlabel('Parameter a')
             ax.legend()
             ax.set_ylabel('P(a|x)')
-            ax.set_title('Probability of a given a uniformly sampled parameter space')
+            ax.set_title(
+                'Probability of a given a uniformly sampled parameter space')
         if mcmc is True:
             ax.plot(np.arange(len(self.chain))+1, self.chain, label='chain')
             ax.axhline(np.e, color='C1', label='True value')
@@ -125,7 +117,7 @@ class CalculateE:
 
     def uniformly_sample_space(self, a_min, a_max, ntrials=1000):
         """
-        uniformaly sample the parameter space from a_min to a_max with ntrails points
+        Uniformaly sample the parameter space from a_min to a_max with ntrails points
         and find the posterior at each point. This method ets the ``.a_array`` and
         ``.posterior`` attributes.
 
@@ -149,7 +141,8 @@ class CalculateE:
         else:
             ntrials = self.ntrails
         self.a_array = np.linspace(a_min, a_max, ntrials)
-        self.posterior = np.array([self.calculate_posterior(a) for a in self.a_array])
+        self.posterior = np.array(
+            [self.calculate_posterior(a) for a in self.a_array])
 
     def run_mcmc(self, a_min, a_max, ntrials=1000):
         """
@@ -177,20 +170,20 @@ class CalculateE:
             ntrials = self.ntrails
         self.chain = self.metropolis_hastings(ntrials, a_min, a_max)
 
-
     def _check_inputs(self, a_min, a_max, ntrials):
         """
         Checks the validity of values input for a_min, a_max, and ntrials.
         """
         assert a_min > 1, 'due to normalization of posterior, a must be greater than 1'
         assert a_min < a_max
-        assert (ntrials > 0) and isinstance(ntrials, int), 'ntrails must be a positive integer'
+        assert (ntrials > 0) and isinstance(
+            ntrials, int), 'ntrails must be a positive integer'
 
     def restart_chain(self):
         """
         Initialize chain to start metropolis hastings with a new starting point
         """
-        self.chain=None
+        self.chain = None
 
     def metropolis_hastings(self, ntrials, a_min, a_max):
         """
@@ -218,17 +211,17 @@ class CalculateE:
         prob_a0 = self.calculate_posterior(a0)
         trial_num = 1
         a_current = a0
-        chain = self.chain or [a_current,]
+        chain = self.chain or [a_current, ]
         prob_a_current = prob_a0
         while trial_num <= ntrials:
             a_trial = np.random.uniform(1, 10, size=1)
             prob_a_trial = self.calculate_posterior(a_trial)
             acceptance_prob = min(1, prob_a_trial/prob_a_current)
             #Accept or reject
-            u = np.random.uniform(0,1)
+            u = np.random.uniform(0, 1)
             if u <= acceptance_prob:
                 a_current = a_trial
                 prob_a_current = prob_a_trial
-            trial_num +=1
+            trial_num += 1
             chain.append(a_current[0])
         return chain
